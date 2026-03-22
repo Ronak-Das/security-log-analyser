c70fcce58f6643464a2151a006a3c71f52738ab1
Ronak Das <ronak12das@gmail.com>
2026-03-22 11:26:21 +0530

    added request validation

--- app/router/logs.py
+++ app/router/logs.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
+from app.models.log_model import Log
 
 router = APIRouter()
 
 @router.post("/logs")
-def ingest_log(log: dict):
+def ingest_log(log: Log):
     return {
         "message": "Log received",
         "data": log
