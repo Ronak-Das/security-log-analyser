6812d6971cfd99585475f7bf362dfe0fb6e950b2
Ronak Das <ronak12das@gmail.com>
2026-03-26 11:19:40 +0530

    Implement in memory storage with multiple log handling

--- app/router/logs.py
+++ app/router/logs.py
@@ -1,11 +1,18 @@
 from fastapi import APIRouter
 from app.models.log_model import Log
 
+logs_storage = []
+
 router = APIRouter()
 
 @router.post("/logs")
 def ingest_log(log: Log):
+    
+    logs_storage.append(log)
     return {
-        "message": "Log received",
-        "data": log
+        "message": "log stored successfully"
     }
+
+@router.get("/logs")
+def get_logs():
+    return logs_storage
