e302b39b6cc79a2d3b8785aa0cafc097aaa86fee
Ronak Das <ronak12das@gmail.com>
2026-04-13 10:57:57 +0530

    added time based detection

--- app/router/logs.py
+++ app/router/logs.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter , Query
 from app.models.log_model import Log
+from datetime import datetime
 
 logs_storage = []
 
@@ -28,12 +29,15 @@ def get_logs(status: str = Query(None), ip: str = Query(None)):
 @router.get("/alerts")
 def detect_suspicious_activity():
     ip_fail_count = {}
+    
+    # Define time window (last 1 minute)
+    time_threshold = datetime.now() - timedelta(minutes=10)
 
     # Count failed attempts per IP
     for log in logs_storage:
-        if log.status == "failed":
+        if log.status == "failed" and log.timestamp >= time_threshold:
             ip_fail_count[log.ip] = ip_fail_count.get(log.ip, 0) + 1
-
+            
     # Find suspicious IPs (more than 3 failures)
     suspicious_ips = [
         {"ip": ip, "failed_attempts": count}
